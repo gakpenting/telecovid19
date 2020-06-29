a2981674c6323a0070246c519c5726bd574964d8
catflip <rinoakbr@gmail.com>
2020-06-29 14:16:06 +0700

    bismillah

---
+++ index.py
@@ -0,0 +1,14 @@
+from telethon.sync import TelegramClient
+from telemongo import MongoSession
+import mongoengine
+import os
+host = os.environ['MONGODB_HOST']
+mongoengine.connect(os.environ['MONGODB_DATABASE'], host=host)
+session = MongoSession(os.environ['MONGODB_DATABASE'], host=host)
+api_id = os.environ['TELEGRAM_API_ID']
+api_hash = os.environ['TELEGRAM_API_HASH']
+client = TelegramClient(session, api_id, api_hash)
+client.connect()
+lonami = client.get_entity('resaqulyubi')
+client.send_message(lonami.id, 'sek gagal tapi res ben send message kudu ngelebokno number')
+# print(lonami.id)
